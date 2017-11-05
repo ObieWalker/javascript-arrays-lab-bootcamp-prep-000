84cc39fdc836deb555b00eff4251eec1623d4a97
Obi <obinnawalker@gmail.com>
2017-11-05 00:24:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var newArray = kittens.unshift(name)
+  var newArray = [...kittens]
+  newArray.push(name)
   return newArray
 }
